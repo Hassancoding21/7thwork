c438ac3b0abb81a616426f23b8f9acb208f41ca2
Hassancoding21 <hhaassaann2100@gmail.com>
2024-02-10 10:04:06 +0500

    all files added

---
+++ app.js
@@ -0,0 +1,36 @@
+// let num1 = "7"
+// let num2= 9
+// num1 = num1 + num2
+// console.log(num1)
+
+// let num3 = 9;
+// num3++;
+// num3++;
+// num3++;
+// num3--;
+// num3++;
+// console.log(num3)
+
+// let totalcost = 7 + 4 / 8
+// console.log(totalcost)
+
+// let math = (7 * 4) + 9
+// console.log(math)
+
+// let name = "hassan"
+// console.log("welcome: " + name + " : how are you")
+
+// let username = prompt("what is you name?");
+// console.log(username)
+
+// let useremail = prompt("what is you email?");
+// console.log(useremail)
+
+// let usernumber = prompt("what is you phone number?");
+// console.log(usernumber)
+
+// let operator = prompt ("whats your operator" ) ;
+// console.log ( 1 operator6 )
+
+
+
